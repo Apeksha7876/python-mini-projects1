96b4500f6e39a3560c97a860ea29ed026af9cdea
Apeksha Gupta <apekshagupta081@gmail.com>
2026-05-19 17:51:52 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+print("1. Add")
+print("2. Subtract")
+
+choice = input("Enter choice: ")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if choice == "1":
+    print("Result:", add(a, b))
+
+elif choice == "2":
+    print("Result:", subtract(a, b))
